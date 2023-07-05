74513de27e93fa5fde67186bd9e15d7d5996e2cb
Jonthan McAvinue <mcavinuejon@gmail.com>
2023-07-05 14:51:40 -0400

    adding fourth round of work

---
+++ TenmoServer/DAO/ITransferDao.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using TenmoServer.Models;
+
+namespace TenmoServer.DAO
+{
+    public interface ITransferDao
+    {
+        List<Transfer> GetTransferByUserId(int id);
+        Transfer GetTransferByStatus(int statusId);
+        Transfer CreateSendTransfer(int receiverUserId);
+        Transfer CreateRequestTransfer(int senderUserId);
+    }
+}
