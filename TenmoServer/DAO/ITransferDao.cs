8dcc06abd70bf1ed89484289712701a7421db542
Jonthan McAvinue <mcavinuejon@gmail.com>
2023-07-05 15:23:48 -0400

    adding fifth round of work

--- TenmoServer/DAO/ITransferDao.cs
+++ TenmoServer/DAO/ITransferDao.cs
@@ -5,7 +5,7 @@ namespace TenmoServer.DAO
 {
     public interface ITransferDao
     {
-        List<Transfer> GetTransferByUserId(int id);
+        IList<Transfer> GetTransferByUserId(int id);
         Transfer GetTransferByStatus(int statusId);
         Transfer CreateSendTransfer(int receiverUserId);
         Transfer CreateRequestTransfer(int senderUserId);
